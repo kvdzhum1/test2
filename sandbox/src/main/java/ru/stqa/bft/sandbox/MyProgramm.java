a28eac824e5caa525558912e591786367145cd7a
kvdzhum1 <60378540+kvdzhum1@users.noreply.github.com>
2020-02-03 23:35:37 +0700

    вычисления всякие

--- sandbox/src/main/java/ru/stqa/bft/sandbox/MyProgramm.java
+++ sandbox/src/main/java/ru/stqa/bft/sandbox/MyProgramm.java
@@ -1,9 +1,23 @@
 package ru.stqa.bft.sandbox;
 
+import java.security.PublicKey;
+
 public class MyProgramm {
   public static void main(String[] args) {
-     System.out.println ("Hello Kostya");
+      hello("Koctya");
+      hello("user");
+      double len =5;
+      System.out.println("Площадь квадрата равна " + area(len)  );
 
   }
 
-}
+     public  static void hello (String sambody){
+          System.out.println ("Hello " + sambody + "!");
+      }
+      public static double area (double l){
+          return l*l;
+      }
+
+
+  }
+
